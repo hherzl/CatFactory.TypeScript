b021ee334ae4617daa8dc5e2ceae670f66de7393
emc2 <ewmccarty@gmail.com>
2019-03-05 15:12:53 -0600

    TSCPath was different on my box. Modified to dynamically find latest known TypeScript version.

--- CatFactory.TypeScript.Tests/ScaffoldingPaths.cs
+++ CatFactory.TypeScript.Tests/ScaffoldingPaths.cs
@@ -1,5 +1,9 @@
 ﻿namespace CatFactory.TypeScript.Tests
 {
+    using System.Linq;
+    using System.Xml.XPath;
+    using Microsoft.EntityFrameworkCore.Internal;
+
     public static class ScaffoldingPaths
     {
         public static string TscPath { get; }
@@ -8,9 +12,27 @@
 
         static ScaffoldingPaths()
         {
-            TscPath = @"C:\Program Files (x86)\Microsoft SDKs\TypeScript\3.0\tsc.exe";
-            TsFilesPath = @"C:\Temp\CatFactory.TypeScript";
-            OutPath = @"c:\Temp\CatFactory.TypeScript\js";
+            ScaffoldingPaths.TscPath = ScaffoldingPaths.WhereTsc();
+            ScaffoldingPaths.TsFilesPath = @"C:\Temp\CatFactory.TypeScript";
+            ScaffoldingPaths.OutPath = @"c:\Temp\CatFactory.TypeScript\js";
+        }
+
+        /// <summary>
+        /// Get the Last Known TypeScript Version
+        /// </summary>
+        /// <returns></returns>
+        static string WhereTsc()
+        {
+            var LastKnownTypeScriptVersion = "3.0";
+            var root = @"C:\Program Files (x86)\Microsoft SDKs\TypeScript";
+            var path = System.IO.Path.Combine(root, @"versions");
+            var di = new System.IO.DirectoryInfo(path);
+            if (di.Exists)
+            {
+                LastKnownTypeScriptVersion = System.Xml.Linq.XDocument.Load
+                    (di.EnumerateFileSystemInfos("*.props").Last().FullName).Root?.Value;
+            }
+            return System.IO.Path.Combine(root, LastKnownTypeScriptVersion, "tsc.exe");
         }
     }
 }
